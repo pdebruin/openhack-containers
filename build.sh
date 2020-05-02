43f0b683d1194db30e3245e73564ab595fdd052a
Ubuntu <pieterd@linuxbuild.5ijgyukandvujin3cgrrbz5iqc.ax.internal.cloudapp.net>
2020-05-02 16:33:55 +0000

    yaml

--- build.sh
+++ build.sh
@@ -15,10 +15,3 @@ cd userprofile
 docker build . -t registryfiw7378.azurecr.io/userprofile:0.9
 cd ..
 cd ..
-
-docker login registryfiw7378.azurecr.io -u registryfiw7378 -p MV9bugQDq=Uivk/CxRwd3kjE3vOJkzLE
-docker push registryfiw7378.azurecr.io/poi:0.9
-docker push registryfiw7378.azurecr.io/trips:0.9
-docker push registryfiw7378.azurecr.io/tripviewer:0.9
-docker push registryfiw7378.azurecr.io/user-java:0.9
-docker push registryfiw7378.azurecr.io/userprofile:0.9
---
+++ push.sh
@@ -0,0 +1,6 @@
+docker login registryfiw7378.azurecr.io -u registryfiw7378 -p MV9bugQDq=Uivk/CxRwd3kjE3vOJkzLE
+docker push registryfiw7378.azurecr.io/poi:0.9
+docker push registryfiw7378.azurecr.io/trips:0.9
+docker push registryfiw7378.azurecr.io/tripviewer:0.9
+docker push registryfiw7378.azurecr.io/user-java:0.9
+docker push registryfiw7378.azurecr.io/userprofile:0.9
