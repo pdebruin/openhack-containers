28cdcc31fab1c73b1911260702273ef929048abf
Ubuntu <pieterd@linuxbuild.5ijgyukandvujin3cgrrbz5iqc.ax.internal.cloudapp.net>
2020-05-03 06:30:38 +0000

    yaml deployments

--- push.sh
+++ push.sh
@@ -1,4 +1,3 @@
-docker login registryfiw7378.azurecr.io -u registryfiw7378 -p MV9bugQDq=Uivk/CxRwd3kjE3vOJkzLE
 docker push registryfiw7378.azurecr.io/poi:0.9
 docker push registryfiw7378.azurecr.io/trips:0.9
 docker push registryfiw7378.azurecr.io/tripviewer:0.9
