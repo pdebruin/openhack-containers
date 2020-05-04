d19aec1aa8c489c5160dd60cc7f0abe895d9ad82
Ubuntu <pieterd@linuxbuild.5ijgyukandvujin3cgrrbz5iqc.ax.internal.cloudapp.net>
2020-05-04 12:09:10 +0000

    expose

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,10 @@ kubectl apply -f trips.yaml -n openhack
 kubectl apply -f tripviewer.yaml -n openhack
 kubectl apply -f user-java.yaml -n openhack
 kubectl apply -f userprofile.yaml -n openhack
-#kubectl expose deployment tripviewer --type=LoadBalancer --name=tripviewer -n openhack --port 80
+
+kubectl expose deployment tripviewer --name=tripviewer --type=LoadBalancer --port 80 -n openhack
+kubectl expose deployment poi --name=poi --type=ClusterIP --port=80 -n openhack
+kubectl expose deployment trips --name=trips --type=ClusterIP --port=80 -n openhack
+kubectl expose deployment user-java --name=user-java --type=ClusterIP --port=80 -n openhack
+kubectl expose deployment userprofile --name=userprofile --type=ClusterIP --port=80 -n openhack
 cd ..
