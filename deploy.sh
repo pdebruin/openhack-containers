44b54058f25ca6a7eb72e10c298c0494e175e995
Ubuntu <pieterd@linuxbuild.5ijgyukandvujin3cgrrbz5iqc.ax.internal.cloudapp.net>
2020-05-07 17:34:54 +0000

    namespaces

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,16 @@
 cd yaml
-kubectl apply -f poi.yaml -n openhack
-kubectl apply -f trips.yaml -n openhack
-kubectl apply -f tripviewer.yaml -n openhack
-kubectl apply -f user-java.yaml -n openhack
-kubectl apply -f userprofile.yaml -n openhack
+kubectl create namespace openhackweb
+kubectl create namespace openhackapi
 
-kubectl expose deployment tripviewer --name=tripviewer --type=LoadBalancer --port 80 -n openhack
-kubectl expose deployment poi --name=poi --type=ClusterIP --port=80 -n openhack
-kubectl expose deployment trips --name=trips --type=ClusterIP --port=80 -n openhack
-kubectl expose deployment user-java --name=user-java --type=ClusterIP --port=80 -n openhack
-kubectl expose deployment userprofile --name=userprofile --type=ClusterIP --port=80 -n openhack
+kubectl apply -f poi.yaml -n openhackapi
+kubectl apply -f trips.yaml -n openhackapi
+kubectl apply -f tripviewer.yaml -n openhackweb
+kubectl apply -f user-java.yaml -n openhackapi
+kubectl apply -f userprofile.yaml -n openhackapi
+
+kubectl expose deployment tripviewer --name=tripviewer --type=LoadBalancer --port 80 -n openhackweb
+kubectl expose deployment poi --name=poi --type=ClusterIP --port=80 -n openhackapi
+kubectl expose deployment trips --name=trips --type=ClusterIP --port=80 -n openhackapi
+kubectl expose deployment user-java --name=user-java --type=ClusterIP --port=80 -n openhackapi
+kubectl expose deployment userprofile --name=userprofile --type=ClusterIP --port=80 -n openhackapi
 cd ..
