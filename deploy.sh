3e1f3f8e200a8d69cceb914576139752b8caae84
Ubuntu <pieterd@linuxbuild.5ijgyukandvujin3cgrrbz5iqc.ax.internal.cloudapp.net>
2020-05-07 18:24:49 +0000

    internal service

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,8 @@ kubectl apply -f tripviewer.yaml -n openhackweb
 kubectl apply -f user-java.yaml -n openhackapi
 kubectl apply -f userprofile.yaml -n openhackapi
 
-kubectl expose deployment tripviewer --name=tripviewer --type=LoadBalancer --port 80 -n openhackweb
+#kubectl expose deployment tripviewer --name=tripviewer --type=LoadBalancer --port 80 -n openhackweb
+kubectl expose deployment tripviewer --name=tripviewer --type=ClusterIP --port 80 -n openhackweb
 kubectl expose deployment poi --name=poi --type=ClusterIP --port=80 -n openhackapi
 kubectl expose deployment trips --name=trips --type=ClusterIP --port=80 -n openhackapi
 kubectl expose deployment user-java --name=user-java --type=ClusterIP --port=80 -n openhackapi
